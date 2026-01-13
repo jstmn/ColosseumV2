8c1d99d5d1530efaa706e7e3f14947f3fdd7f797
Prajwal-Vijay <prajvi.10@gmail.com>
2026-01-13 10:28:50 +0530

    Created all bimanual envs

--- mani_skill/examples/motionplanning/dual_panda/solutions/bimanual_stack-cubes.py
+++ mani_skill/examples/motionplanning/dual_panda/solutions/bimanual_stack-cubes.py
@@ -2,18 +2,16 @@ import gymnasium as gym
 import numpy as np
 import sapien
 import time
-
-import trimesh
 from mani_skill.examples.motionplanning.dual_panda.motionplanner import DualPandaMotionPlanningSolver
-from mani_skill.envs.tasks import DualArmPickBottleEnv
+from mani_skill.envs.tasks import TwoRobotStackCube
 from mani_skill.examples.motionplanning.base_motionplanner.utils import compute_grasp_info_by_obb, get_actor_obb
 
 def main():
     """
     Test the dual panda motion planner with various scenarios.
     """
-    env:DualArmPickBottleEnv = gym.make(
-        'DualArmPickBottle-v0',
+    env:TwoRobotStackCube = gym.make(
+        'TwoRobotStackCube-v1',
         obs_mode='none',
         control_mode="pd_joint_pos",  # Use pd_joint_pos for motion planning
         render_mode='human',  # Use 'human' for visualization
@@ -32,7 +30,7 @@ def main():
     env.close()
     print("\n=== All tests completed ===")
 
-def solve(env:DualArmPickBottleEnv, seed, debug, vis):
+def solve(env:TwoRobotStackCube, seed, debug, vis):
     env.reset(seed=seed)
     
     planner = DualPandaMotionPlanningSolver(
@@ -41,8 +39,7 @@ def solve(env:DualArmPickBottleEnv, seed, debug, vis):
         vis=vis,
         print_env_info=True
     )
-    
-        
+
     # Get initial poses
     tcp_1_pose = env.unwrapped.agent.tcp_1_pose
     tcp_2_pose = env.unwrapped.agent.tcp_2_pose
@@ -63,39 +60,58 @@ def solve(env:DualArmPickBottleEnv, seed, debug, vis):
         q1 = np.array(tcp_1_pose.q).flatten()
         q2 = np.array(tcp_2_pose.q).flatten()
 
-
     FINGER_LENGTH = 0.025
     env = env.unwrapped
-    obb = get_actor_obb(env.obj)
-    approaching = np.array([0, 1, 0])
+
+    # retrieves the object oriented bounding box (trimesh box object)
+    obb_A = get_actor_obb(env.cubeA)
+    obb_B = get_actor_obb(env.cubeB)
+    
+    approaching = np.array([0, 0, -1])
     # get transformation matrix of the tcp pose, is default batched and on torch
     target_closing = env.agent.tcp_1.pose.to_transformation_matrix()[0, :3, 1].cpu().numpy()
     # we can build a simple grasp pose using this information for Panda
     grasp_info = compute_grasp_info_by_obb(
-        obb,
+        obb_A,
         approaching=approaching,
         target_closing=target_closing,
         depth=FINGER_LENGTH,
     )
     closing, center = grasp_info["closing"], grasp_info["center"]
-    
-    grasp_pose = env.agent.build_grasp_pose(approaching, closing, env.obj.pose.sp.p)
+    grasp_A_pose = env.agent.build_grasp_pose(approaching, closing, env.cubeA.pose.sp.p)
 
-    grasp_1_approach_pose = grasp_pose*sapien.Pose(p=[0.15,0,-0.1])
-    grasp_1_approach_pose.q = np.array([-0.5,0.5,0.5,0.5])
-    result = planner.move_to_pose_with_screw(
-        grasp_1_approach_pose,  # left
-        arm_index=1
+    target_closing = env.agent.tcp_2.pose.to_transformation_matrix()[0, :3, 1].cpu().numpy()
+    # we can build a simple grasp pose using this information for Panda
+    grasp_info = compute_grasp_info_by_obb(
+        obb_B,
+        approaching=approaching,
+        target_closing=target_closing,
+        depth=FINGER_LENGTH,
+    )
+    closing, center = grasp_info["closing"], grasp_info["center"]
+    grasp_B_pose = env.agent.build_grasp_pose(approaching, closing, env.cubeB.pose.sp.p)
+
+    place_A_pose = sapien.Pose(p=np.array(env.goal_region.pose.p[0]), q=grasp_A_pose.get_q())
+    place_A_pose = place_A_pose * sapien.Pose(p=[0,0,-0.1])
+    
+    place_B_pose = sapien.Pose(p=np.array(env.goal_region.pose.p[0]), q=grasp_B_pose.get_q())
+    place_B_pose = place_B_pose * sapien.Pose(p=[0,0,-0.1])
+
+    grasp_A_approach_pose = grasp_A_pose*sapien.Pose(p=[0,0,-0.1])
+    grasp_B_approach_pose = grasp_B_pose*sapien.Pose(p=[0,0,-0.1])
+    
+    result = planner.move_to_pose_pair_with_screw(
+        grasp_B_approach_pose,
+        grasp_A_approach_pose  # left
     )
 
     if result==-1:
         print("Failed grasp_approach")
         return False
-    grasp_pose = grasp_pose*sapien.Pose(p=[0.14,0,0.00])
-    grasp_pose.q = np.array([-0.5,0.5,0.5,0.5])
-    result = planner.move_to_pose_with_screw(
-        grasp_pose,  # left
-        arm_index=1
+    
+    result = planner.move_to_pose_pair_with_screw(
+        grasp_B_pose,
+        grasp_A_pose  # left
     )
 
     if result==-1:
@@ -103,60 +119,46 @@ def solve(env:DualArmPickBottleEnv, seed, debug, vis):
         return False
     
     planner.close_gripper(arm_index=1, t=10)
+    planner.close_gripper(arm_index=2, t=10)
     
-    print("\n5. Lifting...")
-    lift_1 = sapien.Pose(
-        p=np.array([-0.333, -0.10, 1.5]),
-        q=np.array([-0.5,0.5,0.5,0.5])
-    )
-    
-    lift_2 = sapien.Pose(
-        p=np.array([-0.333, 0.10, 1.48]),
-        q=np.array([0.5,0.5,0.5,-0.5])
-    )
-    
-    result = planner.move_to_pose_pair_with_RRTConnect(
-        lift_2,  # left
-        lift_1
-        # refine_steps=5
-    )
-    
-    if result == -1:
-        print("Failed to lift")
-        return False
-    
-    # 5. Lift up
-    print("\n5. Lifting...")
-    lift_1 = sapien.Pose(
-        p=np.array([-0.333, 0.04, 1.5]),
-        q=np.array([-0.5,0.5,0.5,0.5])
-    )
-    
-    lift_2 = sapien.Pose(
-        p=np.array([-0.333, 0.04, 1.48]),
-        q=np.array([0.5,0.5,0.5,-0.5])
-    )
-    
-    result = planner.move_to_pose_pair_with_screw(
-        lift_2,  # left
-        lift_1,  # right
-        # refine_steps=5
-    )
-    
-    if result == -1:
-        print("Failed to lift")
-        return False
-    
-            # 6. Open grippers
-    print("\n6. Releasing...")
-    planner.close_gripper(arm_index=2, t=5)
-    planner.open_gripper(arm_index=1, t=5)
-    lift_2 = sapien.Pose(
-        p=np.array([-0.333, 0.20, 1.48]),
-        q=np.array([0.5,0.5,0.5,-0.5])
-    )
+    place_A_lift_pose = place_A_pose * sapien.Pose(p=[0,0,-0.1])
+    # life A above target
     result = planner.move_to_pose_with_screw(
-        lift_2,
+        place_A_lift_pose,
+        arm_index=1
+    )
+    
+    if result == -1:
+        print("Failed to lift")
+        return False
+    
+    # Place A
+    result = planner.move_to_pose_with_screw(
+        place_A_pose,
+        arm_index=1
+    )
+    
+    if result == -1:
+        print("Failed to lift")
+        return False
+    
+    planner.open_gripper(arm_index=1, t=10)
+
+    final_A_pose = sapien.Pose(p=[-0.4,-0.254,1.018],q=[0.138,0.694,0.694,-0.138])
+    result = planner.move_to_pose_with_screw(
+        final_A_pose,  # left
+        arm_index=1
+        # refine_steps=5
+    )
+    
+    if result == -1:
+        print("Failed to Reset arm A")
+        return False
+    
+    # Place B
+    place_B_pose = place_B_pose*sapien.Pose(p=[0,0,-0.1])
+    result = planner.move_to_pose_with_screw(
+        place_B_pose,
         arm_index=2
     )
     
@@ -164,22 +166,10 @@ def solve(env:DualArmPickBottleEnv, seed, debug, vis):
         print("Failed to lift")
         return False
     
-    grasp_1 = sapien.Pose(
-        p=np.array([-0.2, -0.141, 1]),
-        q=grasp_pose.q
-    )
-    grasp_2 = sapien.Pose(
-        p=np.array([0.1, 0.141, 1]),
-        q=lift_2.q
-    )
-
-    result = planner.move_to_pose_pair_with_screw(
-        grasp_2,  # left
-        grasp_1,  # right
-        # refine_steps=5
-    )
+    planner.open_gripper(arm_index=2, t=10)
     
     return True
+
 
 if __name__ == "__main__":
     main()
