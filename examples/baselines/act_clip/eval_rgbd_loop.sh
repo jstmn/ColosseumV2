d779af0c02d1e3d32105f5d3cb824a1b037493e3
Jeremy Morgan <jsmorgan6@gmail.com>
2026-02-24 18:21:44 -0800

    tweaks

--- examples/baselines/act_clip/eval_rgbd_loop.sh
+++ examples/baselines/act_clip/eval_rgbd_loop.sh
@@ -3,16 +3,82 @@
 source ~/miniconda3/etc/profile.d/conda.sh
 conda activate maniskill_jm
 
+
+# ============================================================
+# Env ID                              | Count     
+# ------------------------------------------------------------
+# DualArmPickCube-v1                  | 98
+# DualArmPickBottle-v1                | 98
+# DualArmLiftPot-v1                   | 98
+# DualArmLiftTray-v1                  | 98
+# DualArmPushBox-v1                   | 96
+# DualArmPourPot-v1                   | 98
+# DualArmThreading-v1                 | 98
+# DualArmPenCap-v1                    | 98
+# DualArmDrawerPlace-v1               | 100       
+# DualArmDrawerOpen-v1                | 100       
+# DualArmStackCube-v1                 | 96        
+# DualArmStack3Cube-v1                | 96        
+# ------------------------------------------------------------
+# Total Merged Episodes               | 1174      
+# Max Episode Length (Steps)          | 265       
+# ============================================================
+
+# ----------------------------------------------------------------
+# ----------------------------------------------------------------
+# Env ID                              | Count     
+# ------------------------------------------------------------
+# PickSodaFromCabinet-v1              | 90
+# PickDishFromRack-v1                 | 90
+# StackCubeColosseumV2-v1             | 90
+# PlaceDishInRack-v1                  | 72
+# LiftPegUprightColosseumV2-v1        | 90
+# RotateArrow-v1                      | 90
+# PegInsertionSideColosseumV2-v1      | 86
+# PlugChargerColosseumV2-v1           | 74
+# HammerNail-v1                       | 82
+# ScoopBanana-v1                      | 88
+# OpenDrawer-v1                       | 90
+# OpenCabinet-v1                      | 73
+# PlaceCubeInDrawer-v1                | 99
+# PlaceBookInShelf-v1                 | 88
+# CookItemInPan-v1                    | 100       
+# RaiseCube-v1                        | 100       
+# ------------------------------------------------------------
+# Total Merged Episodes               | 1402      
+# Max Episode Length (Steps)          | 702       
+# ============================================================
+
+
+
 while true; do
-    python examples/baselines/act/eval_rgbd.py \
-        --checkpoint-path checkpoints/best_eval_success_once__BIMANUAL_JAN30.pt \
+    # python examples/baselines/act_clip/eval_rgbd.py \
+    #     --checkpoint-path checkpoints/hyeonho_simul_results/Multi-task_single_lang/best_eval_success_once.pt \
+    #     --control-mode "pd_ee_delta_pose" \
+    #     --no-include-depth \
+    #     --sim-backend "physx_cuda" \
+    #     --is-multi-task True \
+    #     --target-num-cams 1 \
+    #     --num-eval-episodes 100 \
+    #     --num-eval-envs 50 \
+    #     --max-episode-steps 350 \
+    #     --hidden-dim 512 --dim-feedforward 1600 --enc-layers 4 --dec-layers 7 \
+    #     --internal-instruction \
+    #     --distraction-set "BLANK" \
+    #     --results-path logs/results_single_arm.csv
+
+    python examples/baselines/act_clip/eval_rgbd.py \
+        --checkpoint-path checkpoints/hyeonho_simul_results/Multi-task_bimanual_lang/best_eval_success_once.pt \
         --control-mode "pd_joint_pos" \
         --no-include-depth \
         --sim-backend "physx_cuda" \
-        --capture-video \
+        --is-multi-task True \
+        --target-num-cams 1 \
         --num-eval-episodes 100 \
         --num-eval-envs 50 \
-        --max-episode-steps 500 \
+        --max-episode-steps 850 \
+        --hidden-dim 512 --dim-feedforward 1600 --enc-layers 4 --dec-layers 7 \
+        --internal-instruction \
         --distraction-set "BLANK" \
         --results-path logs/results_bimanual.csv
 done
