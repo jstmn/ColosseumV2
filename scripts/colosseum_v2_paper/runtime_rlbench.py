91202fdae9e88a943497adbfc02b23d71eb16244
Jeremy Morgan <jsmorgan6@gmail.com>
2026-03-31 15:55:49 -0700

    tweaks

--- scripts/colosseum_v2_paper/runtime_rlbench.py
+++ scripts/colosseum_v2_paper/runtime_rlbench.py
@@ -14,10 +14,6 @@ from typing import Any
 from rlbench.action_modes.action_mode import JointPositionActionMode
 import numpy as np
 
-import sys
-sys.path.append("examples/baselines/act_clip")
-from eval_rgbd import MAX_EPISODE_STEPS_BY_TASK
-
 
 """
 This script measures maniskill's FPS for difference batch sizes. Note that you need to run each environment in a 
@@ -37,8 +33,8 @@ source ~/.bashrc
 pip install git+https://github.com/stepjam/RLBench.git
 
 # Example usage:
-python scripts/colosseum_v2_paper/runtime_rlbench_2.py \
-    --n_steps 200
+python scripts/colosseum_v2_paper/runtime_rlbench.py \
+    --results_filepath "logs/fps/rlbench_fps.csv" --batch_size 10
 
 """
 
@@ -54,16 +50,110 @@ def measure_runtime(
     )
     env.reset()
 
+    raw_results[process_id] = {
+        "started": True,
+        "timing_data": None
+    }
+    timing_data = {i: 0.0 for i in range(n_steps)}
+
     for i in range(n_steps):
         env.step(env.action_space.sample())
         # This dict setting is counted as part of the computation time which is slightly unfair, but in practice it's
         # runs in microseconds so it's negligible compared to the 10+ seconds from RLBench.
+        timing_data[i] = time()
+
+    raw_results[process_id] = {"started": True, "timing_data": timing_data}
     env.close()
-    tf = time()
-    print(f"Time taken:")
-    print(f"  - seconds: {tf - t0}")
-    print(f"  - minutes: {round((tf - t0) / 60, 5)}")
-    print(f"  - hours:   {round((tf - t0) / 3600, 5)}")
+    print(f"process {process_id} finished")
+
+
+
+def measure_runtime_fps(
+    results_filepath: str,
+    batch_size: int,
+    n_steps: int,
+    env_id: str = "rlbench/slide_block_to_target-vision-v0",
+):
+
+    ctx = mp.get_context("spawn")
+    manager = ctx.Manager()
+    raw_results = manager.dict()
+
+    procs: list[tuple[int, Any]] = []
+    for process_id in range(batch_size):
+        p = ctx.Process(
+            target=_measure_fps_subprocess,
+            args=(raw_results, process_id, env_id, n_steps),
+            daemon=False,
+        )
+        p.start()
+        procs.append((process_id, p))
+
+    # measure cpu usage
+    while True:
+        for process_id, result in raw_results.items():
+            if "started" in result and not result["started"]:
+                sleep(0.01)
+                print("waiting for process ", process_id, " to start")
+                continue
+        print("all processes started")
+        break
+    sleep(1.0)
+    usage = psutil.cpu_percent(percpu=True, interval=1.0)
+    print("cpu usage: ", usage)
+
+    #
+    for process_id, p in procs:
+        p.join()
+
+
+    # We need to do something tricky here. We only want to measure the fps when all environments are running at the same
+    # time. To do this, we need to find the minimum t0 and the maximum t1 where between these two timestamps, all
+    # environments are running. After doing this, we need to calculate how many steps were taken in each environment.
+    min_t0 = float('inf')
+    max_t1 = float('-inf')
+    for process_id, result in raw_results.items():
+        assert result["started"], f"process {process_id} did not start"
+        assert result["timing_data"] is not None, f"process {process_id} did not return timing data"
+        timing_data = result["timing_data"]
+        for i in range(n_steps):
+            if timing_data[i] < min_t0:
+                min_t0 = timing_data[i]
+            if timing_data[i] > max_t1:
+                max_t1 = timing_data[i]
+    print("min_t0: ", min_t0, "max_t1: ", max_t1, "time_delta: ", max_t1 - min_t0)
+    assert min_t0 < max_t1
+
+    n_steps_taken = 0
+    for process_id, data in raw_results.items():
+        timing_data = data["timing_data"]
+        for i in range(n_steps):
+            if timing_data[i] > min_t0 and timing_data[i] < max_t1:
+                n_steps_taken += 1
+    fps = n_steps_taken / (max_t1 - min_t0)
+    seconds_per_frame = 1 / fps
+
+    print("new data: ", batch_size, fps, seconds_per_frame)
+    print()
+
+    DF_COLS = ["batch_size", "frames_per_second", "seconds_per_frame", "per_core_cpu_usage"]
+    if os.path.exists(results_filepath):
+        df = pd.read_csv(results_filepath)
+        assert list(df.columns) == DF_COLS, f"CSV columns must be exactly {DF_COLS} (in order), got {list(df.columns)}"
+    else:
+        df = pd.DataFrame(columns=pd.Index(DF_COLS))
+
+    df.loc[len(df)] = {
+        "batch_size": batch_size,
+        "frames_per_second": fps,
+        "seconds_per_frame": seconds_per_frame,
+        "per_core_cpu_usage": usage,
+        "average_cpu_usage": np.mean(usage),
+    }
+    df.to_csv(results_filepath, index=False)
+    print(df)
+    print(f"Saved to {results_filepath}")
+
 
 
 if __name__ == "__main__":
@@ -71,4 +161,6 @@ if __name__ == "__main__":
     parser.add_argument("--env_id", type=str, default="rlbench/slide_block_to_target-vision-v0")
     parser.add_argument("--n_steps", type=int, required=True)
     args = parser.parse_args()
-    measure_runtime(n_steps=args.n_steps, env_id=args.env_id)
+    Path(args.results_filepath).parent.mkdir(parents=True, exist_ok=True)
+
+    measure_runtime_fps(args.results_filepath, args.batch_size, env_id=args.env_id, n_steps=args.n_steps)
